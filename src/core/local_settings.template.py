383ae9ecd214e819d2311e15d14ddba68876334e
toplenboren <toplenboren@yandex.ru>
2020-07-09 16:51:39 +0500

    Add simpleJWT settings and remove comments 🐃

--- src/core/local_settings.template.py
+++ src/core/local_settings.template.py
@@ -1,4 +1,6 @@
 import os
+from datetime import timedelta
+
 from django.conf import settings
 
 DEBUG = True
@@ -8,6 +10,11 @@ ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000' # the domain for front-end app(you can add more than 1)
 ]
+
+SIMPLE_JWT = { # https://django-rest-framework-simplejwt.readthedocs.io/en/latest/settings.html
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=500),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=100)
+}
 
 DATABASES = {
     'default': {
